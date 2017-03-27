7f560b4dae4c8c22b4bf0b04ad5636c33871af60
Brittany Picazo <britpicazo@gmail.com>
2017-03-26 23:55:41 -0500

    3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,8 @@ var numWins;
 var getNewWord;
 var wordPlace; //place in breeds array
 var correctGuesses; 
-
+var updatedWord;
+var wordAsArr=[];
 
 function initialize() {
 	gameStarted = true;
@@ -20,9 +21,11 @@ function initialize() {
 	guessesLeft = 10;
 	lettersGuessed = [];
 	numWins++;
+	correctGuesses = 0;
 	wordPlace = Math.floor(Math.random() * 36);
 	currentWord = breeds[wordPlace];
 	wordAsDashes = makeIntoDashes(currentWord);
+	wordAsArr =  makeIntoArray(currentWord);
 }
 
 function makeIntoDashes(word){
@@ -34,15 +37,61 @@ function makeIntoDashes(word){
 	return dashes;
 }
 
+function makeIntoArray(currentWord){
+	var arr=[];
+	for(i = 0; i < currentWord.length; i++){
+		arr.push(currentWord[i]);
+	}
+	return arr;
+}
+
+function playGame(){
+	if(correctGuesses < currentWord.length || guessesLeft > 0){
+		document.onkeyup = function(event){
+			var letter = event.key.toLowerCase();
+			if(alphabet.indexOf(letter) > -1){
+				if(wordAsArr.indexOf(letter) > -1){
+					correctGuesses++;
+					displayLetter(wordAsArr.indexOf(letter));
+				}
+				else{
+					guessesLeft--;
+					document.getElementById("guessesLeft").innerHTML = guessesLeft;
+					lettersGuessed.push[letter];
+					document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+
+				}
+			}
+		}
+	}
+	initialize();
+}
+
+function displayLetter(indOfLetter){
+	var updatedWord = "";
+	for(i = 0; i < currentWord.length - 1; i++){
+		if(parseInt(indOfLetter) == i && i != currentWord.length -1){
+			updatedWord += currentWord[i] + " ";
+		}
+		else if(parseInt(indOfLetter) == i && i == currentWord.length -1){
+			updatedWord += currentWord[i];
+		}
+		else if(parseInt(indOfLetter) != i && i == currentWord.length -1){
+			updatedWord += "_";
+		}
+		else{
+			updatedWord += "_ ";
+		}
+	}
+	document.getElementById("currentWord").innerHTML = updatedWord;
+}
 
 document.onkeyup = function(event){
 	if(!gameStarted){
 		document.getElementById("letsPlay").innerHTML = "Let's Play!!";
 		initialize();
-		console.log(currentWord);
-		console.log(wordAsDashes);
 		document.getElementById("currentWord").innerHTML = wordAsDashes;
-		// playGame();
+		playGame();
 
 	}
 }
