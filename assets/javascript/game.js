9662fe69e2234d5c34b17341593738c1fc14d36d
Brittany Picazo <britpicazo@gmail.com>
2017-10-18 14:34:39 -0500

    game fully functional now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,104 +1,107 @@
-
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var breeds = ["affenpinscher", "akita", "basenji", "beagle", "beauceron",   "bloodhound", "borzoi", "boxer","briard", "brittany", "bulldog", "bullmastiff","chihuahua", "collie", "dachshund", "dalmatian", "greyhound", "havanese", "leonberger", "maltese","mastiff", "newfoundland", "poodle", "pug", "retriever", "rottweiler", "saluki","samoyed", "schipperke", "setter","sloughi", "spaniel", "vizsla", "weimaraner", "whippet", "xoloitzcuintil"];
+var breeds = ["affenpinscher", "akita", "basenji", "beagle", "beauceron", "bloodhound", "borzoi", "boxer", "briard", "brittany", "bulldog", "bullmastiff", "chihuahua", "collie", "dachshund", "dalmatian", "greyhound", "havanese", "leonberger", "maltese", "mastiff", "newfoundland", "poodle", "pug", "retriever", "rottweiler", "saluki", "samoyed", "schipperke", "setter", "sloughi", "spaniel", "vizsla", "weimaraner", "whippet", "xoloitzcuintil"];
 
-var gameStarted=false;
+var gameStarted = false;
 var currentWord;
 var wordAsDashes;
 var guessesLeft;
 var lettersGuessed;
-var numWins;
+var numWins = 0;
+var numLosses = 0;
 var getNewWord;
 var wordPlace; //place in breeds array
-var correctGuesses; 
-var wordAsArr=[];
+var correctGuesses;
+var wordAsArr = [];
 var dashesArray = [];
 
 function initialize() {
 	gameStarted = true;
-	currentWord = "";
-	guessesLeft = 10;
 	lettersGuessed = [];
-	numWins++;
 	correctGuesses = 0;
 	wordPlace = Math.floor(Math.random() * 36);
-	currentWord = breeds[wordPlace];				//string
-	wordAsDashes = makeIntoDashes(currentWord);		//string
-	wordAsArr =  makeIntoArray(currentWord);		//array
-	dashesArray = makeIntoArray(wordAsDashes);		//array
+	currentWord = breeds[wordPlace];			//string
+	guessesLeft = 17 - currentWord.length;		//longer words get less guesses
+	wordAsDashes = makeIntoDashes(currentWord);	//string of dashes
+	wordAsArr = currentWord.split('');			//array with letters
+	dashesArray = wordAsDashes.split('');		//array with dashes
+	document.getElementById("currentWord").innerHTML = wordAsDashes;
+	document.getElementById("lettersGuessed").innerHTML = "--";
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 }
 
-function makeIntoDashes(word){
+// Make each word into underscores, visually like hangman
+function makeIntoDashes(word) {
 	var dashes = "";
-	for(i = 0; i < word.length - 1; i++){
+	for (i = 0; i < word.length - 1; i++) {
 		dashes += "_ ";
 	}
 	dashes += "_";
 	return dashes;
 }
 
-function makeIntoArray(currentWord){
-	var arr=[];
-	for(i = 0; i < currentWord.length; i++){
-		arr.push(currentWord[i]);
-	}
-	return arr;
-}
+// Main function that controls what to do with each keystroke
+function playGame(letter) {
+	var letter = letter.toLowerCase();
 
-function playGame(letter){
-	if(guessesLeft == 0)
-	{
-		initialize();
-	} 
-	else if(correctGuesses == currentWord.length){
-		numWins++;
-		document.getElementById("numWins").innerHTML = numWins;
-		initialize();
-	}
-
-	else{
-		// document.onkeyup = function(event){
-			var letter = letter.toLowerCase();
-			console.log(letter);
-			if(alphabet.indexOf(letter) > -1){
-				if(wordAsArr.indexOf(letter) > -1){
-					correctGuesses++;
-					displayLetter(letter);
-				}
-				else{
-					if(lettersGuessed.indexOf(letter) > -1){
-						return;
-					}
-					else{
-						guessesLeft--;
-						document.getElementById("guessesLeft").innerHTML = guessesLeft;
-						lettersGuessed.push[letter];
-						document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-					}
+	// Checks if key is a letter
+	if (alphabet.indexOf(letter) > -1) {
+		if (wordAsArr.indexOf(letter) > -1) {
+			correctGuesses++;
+			displayLetter(letter);
+		}
+		else {
+			if (lettersGuessed.indexOf(letter) > -1) {
+				return;
+			}
+			else {
+				guessesLeft--;
+				document.getElementById("guessesLeft").innerHTML = guessesLeft;
+				lettersGuessed.push(letter);
+				document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(' ');
+				if (guessesLeft == 0) {
+					alert("Sorry! The correct answer is " + currentWord);
+					initialize();
+					numLosses++;
+					document.getElementById("losses").innerHTML = numLosses;
 				}
 			}
-		// }
-	}
-	initialize();
-
-}
-
-function displayLetter(letter){
-	//for each char in wordAsDashes, if matches currentWord --> display
-	for(i = 0; i < currentWord.length; i++){
-		if(letter == currentWord.charAt[i]){
-			dashesArray[i] = currentWord.charAt[i];
 		}
 	}
 }
 
-document.onkeyup = function(event){
-	if(!gameStarted){
-		document.getElementById("letsPlay").innerHTML = "Let's Play!!";
-		initialize();
-		document.getElementById("currentWord").innerHTML = wordAsDashes;
-		console.log(currentWord);		
+// Displays letter if it's in word
+function displayLetter(letter) {
+	// for each char in wordAsDashes, if matches currentWord --> display
+	for (i = 0; i < currentWord.length; i++) {
+		if (letter == wordAsArr[i]) {
+			dashesArray[i * 2] = letter;
+			console.log(dashesArray);
+		}
 	}
-	playGame(event.key);
+	document.getElementById("currentWord").innerHTML = dashesArray.join("");
+	checkForWin();
+}
+
+// Checks for win by looking for "_"
+function checkForWin() {
+	if (dashesArray.indexOf("_") === -1) {
+		alert("You got it! The correct answer is " + currentWord);
+		numWins++;
+		document.getElementById("wins").innerHTML = numWins;
+		initialize();
+	}
+}
+
+document.onkeyup = function (event) {
+	if (!gameStarted) {
+		document.getElementById("letsPlay").innerHTML = "";
+		initialize();
+		document.getElementById("currentWord").innerHTML = wordAsDashes.split(",");
+		console.log(currentWord);
+		gameStarted = true;
+	}
+	else {
+		playGame(event.key);
+	}
 }
